3d5188b26403812aff55caee725d3c14a7c36763
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-06-09 13:04:58 +0300

    Student.java

--- src/main/java/com/hostel/model/Student.java
+++ src/main/java/com/hostel/model/Student.java
@@ -1,4 +1,43 @@
 package com.hostel.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Student extends User {
+
+    private String gender;
+
+    // Student can have many bookings
+    @OneToMany
+    private List<Booking> bookings;
+
+    //constructor
+    public Student(int id, String name, String email, String password, String gender) {
+        super(id, name, email, password);
+        this.gender = gender;
+    }
+
+    // Getters and Setters
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
 }
+
+
+
+}
+
