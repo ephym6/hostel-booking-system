2da2777f976563ae2ca8740178e73cb2c0f63203
nicole205dev <nicole.kemunto@strathmore.edu>
2025-06-09 03:09:48 +0300

    Initial commit

--- src/main/java/com/hostel/model/Student.java
+++ src/main/java/com/hostel/model/Student.java
@@ -1,7 +1,38 @@
 package com.hostel.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import java.util.List;
 
 @Entity
 public class Student extends User {
+
+    private String gender;
+
+    // Student can have many bookings
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    // Getters and Setters
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+}
+}
+
+
+
 }
