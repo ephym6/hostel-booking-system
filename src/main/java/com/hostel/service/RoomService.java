f29bbf5656d66dec10e68b567995eed6543b4799
anniewanjohi <annieww24@gmail.com>
2025-07-02 22:47:10 +0300

    RoomController,RoomService,RoomServiceImpl

--- src/main/java/com/hostel/service/RoomService.java
+++ src/main/java/com/hostel/service/RoomService.java
@@ -1,4 +1,9 @@
 package com.hostel.service;
 
+import com.hostel.model.Room;
+import java.util.List;
+
 public interface RoomService {
+    List<Room> getAllRooms();
+    List<Room> getAvailableRooms();
 }
