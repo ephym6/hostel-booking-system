a47f8d91b6f8f7613d569fd5f8d1ef2ab77c5f1b
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-06 22:09:43 +0300

    Notification Controller

--- src/main/java/com/hostel/controller/NotificationController.java
+++ src/main/java/com/hostel/controller/NotificationController.java
@@ -1,4 +1,25 @@
 package com.hostel.controller;
 
+import com.hostel.model.Notification;
+import com.hostel.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notifications")
 public class NotificationController {
+    @Autowired
+    private NotificationService notificationService;
+
+    @GetMapping("/unseen/{studentId}")
+    public ResponseEntity<?> getUnseen(@PathVariable Long studentId) {
+        List<Notification> unseen = notificationService.getUnseenNotifications(studentId);
+        return ResponseEntity.ok(unseen);
+    }
 }
