d8b3f6e774ecec86301a27da430d5ccbe18909e3
ephym6 <ephy.macharia.mbuu@gmail.com>
2025-07-06 23:53:08 +0300

    Added view controller to map html pages to spring backend

---
+++ src/main/java/com/hostel/controller/ViewController.java
@@ -0,0 +1,37 @@
+package com.hostel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/book-room")
+    public String bookRoomPage() {
+        return "book-room";
+    }
+
+    @GetMapping("/browse-rooms")
+    public String browseRoomsPage() {
+        return "browse-rooms";
+    }
+
+    @GetMapping("/admin-dashboard")
+    public String adminDashboard() {
+        return "admin-dashboard";
+    }
+}
